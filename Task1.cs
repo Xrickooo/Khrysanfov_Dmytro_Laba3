8c44d2782dff648e3b579916ea9e2ee771c0d02e
Xrickooo <89274575+Xrickooo@users.noreply.github.com>
2022-05-18 22:52:48 +0300

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace Laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Створити об'єкт класу Держава, використовуючи класи Область, Район, Місто.
+            //Методи: вивести на консоль столицю, кількість областей, площу, обласні центри.
+            var country = new Country
+            {
+                name = "Ukraine"
+            };
+            var regions = new Country.Regions
+            {
+                regions = new string[]
+                {
+                    "Kyiv region", "Sumy region", "Kharkiv region", "Lviv region", "Donetsk region"
+                }
+            };
+            var count = new Country.Regions
+            {
+                count = regions.regions.Length
+            };
+            var cities = new Country.Cities
+            {
+                cities = new string[]
+                {
+                    "Kyiv", "Sumy", "Kharkivn", "Lviv", "Donetsk", "Dnipro"
+                }
+            };
+            var count2 = new Country.Cities
+            {
+                count2 = cities.cities.Length
+            };
+            var districts = new Country.District
+            {
+                squares = new int[]
+                {
+                    5, 6, 7, 10, 12, 7, 8, 4, 9
+                }
+            };
+            GetCapital();
+            GetRegNum();
+            GetSquare();
+            GetCities();
+            GetInfo();
+            void GetInfo()
+            {
+                Console.WriteLine(country.ToString());
+
+                bool regcount = count.Equals(count2);
+                Console.WriteLine(regcount);
+                Console.WriteLine(count.GetHashCode());
+
+                bool citycount = count2.Equals(districts.squares.Length);
+                Console.WriteLine(citycount);
+                Console.WriteLine(count2.GetHashCode());
+
+            }
+            void GetCapital()
+            {
+                Console.WriteLine($"Capital is {cities.cities[0]}");
+            }
+            void GetRegNum()
+            {
+                Console.WriteLine($"Number of regions is {count.count}");
+            }
+            void GetSquare()
+            {
+                int s = 0;
+                for (int i = 0; i < districts.squares.Length; i++)
+                {
+                    s += districts.squares[i];
+                }
+                Console.WriteLine($"Square of country is {s}");
+            }
+            void GetCities()
+            {
+                Console.WriteLine("Region's centers:");
+                for (int i = 0; i < count2.count2; i++)
+                {
+                    Console.WriteLine(cities.cities[i]);
+                }
+            }
+        }
+    }
+    class Country
+    {
+        public string name { get; set; }
+        public override string ToString()
+        {
+            return $"Country: {name}";
+        }
+        public class Regions
+        {
+            public string[] regions { get; set; }
+            public int count { get; set; }
+            public override bool Equals(object obj)
+            {
+                if (obj is Cities reg) return count == reg.count2;
+                return false;
+            }
+            public override int GetHashCode() => count.GetHashCode();
+        }
+        public class District
+        {
+            public int[] squares { get; set; }
+        }
+        public class Cities
+        {
+            public string[] cities { get; set; }
+            public int count2 { get; set; }
+            public override bool Equals(object obj)
+            {
+                if (obj is District ct) return count2 == ct.squares.Length;
+                return false;
+            }
+            public override int GetHashCode() => count2.GetHashCode();
+        }
+    }
+}
---
+++ Task2.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace laba3
+{
+     class SortStation // Создаём класс сортировочной станции
+    {
+        public  List<Garbage> AllObjects = new List<Garbage>();
+
+         public void sort(List<Garbage> AllObjects) // метод сортировки мусора по весу
+
+        {
+            Console.WriteLine(new String('-', 50));
+            AllObjects.Sort((x, y) => x.weight > y.weight ? -1 : 1);
+            Console.WriteLine("Сортировка мусора (по весу) ");
+            foreach (var obj in AllObjects)
+                Console.WriteLine(obj.name + " " + obj.weight);
+            AllObjects.Sort((x, y) => x.weight > y.weight ? 1 : -1);
+            Console.WriteLine(new String('-', 50));
+        }
+
+        public void sortGarbageTYPES(List<Garbage> AllObjects) // метод сортировки мусора по классам мусора
+        {
+            var musor = AllObjects.OrderBy(p => p.material);
+
+            foreach (var p in musor)
+                Console.WriteLine($"{p.material} - {p.name}");
+
+            Console.WriteLine(new String('-', 50));
+        }
+
+        public void sortGarbagebyweight(List<Garbage> AllObjects)  // вывод мусора, вес которых меньше 300 г
+        {
+            foreach (var obj in AllObjects)
+            {
+                if (obj.weight < 300)
+                    Console.WriteLine("Предметы, вес которых меньше 300 г - " + obj.name);
+            }
+            Console.WriteLine(new String('-', 50));
+        }
+    }
+    class Garbage : SortStation  // Создаём класс мусора
+    {
+        public string name { get; set; }
+        public int weight { get; set; }           // конструктор мусора
+        public string material { get; set; }
+
+    }
+
+
+         class Glass : Garbage { } // создаём классы видов мусора, наследуя от базового
+         class Plastic : Garbage { }
+         class Paper : Garbage { }
+         class Other : Garbage { }
+
+
+
+
+        class Go
+        {
+            public static void Main()
+            {
+            
+
+            var banana = new Garbage
+                {
+                    name = "Банан",
+                    weight = 90,
+                    material = "Other",
+                };
+
+                var cup = new Garbage
+                {
+                    name = "Чашка",
+                    weight = 300,                          // создаём разные экземпляры мусора
+                    material = "Glass",
+                };
+
+                var bottle = new Garbage
+                {
+                    name = "Бутылка",
+                    weight = 50,
+                    material = "Plastic",
+                };
+
+                var karton = new Garbage
+                {
+                    name = "Картон",
+                    weight = 5,
+                    material = "Paper",
+                };
+
+                var chair = new Garbage
+                {
+                    name = "Стул",
+                    weight = 1000,
+                    material = "Other",
+                };
+
+            var station = new SortStation { };
+
+            station.AllObjects.Add(chair);
+            station.AllObjects.Add(banana);
+            station.AllObjects.Add(cup);      // добавляем весь в мусор в лист
+            station.AllObjects.Add(bottle);
+            station.AllObjects.Add(karton);
+            station.sort(station.AllObjects);
+            station.sortGarbageTYPES(station.AllObjects);
+            station.sortGarbagebyweight(station.AllObjects);
+
+
+
+        }
+
+
+        }
+    
+}
